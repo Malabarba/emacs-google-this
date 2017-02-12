c5b253aff0423ca4cefea36b18fe39a0f9dab9aa
Jonas Bernoulli <jonas@bernoul.li>
2017-02-12 13:26:21 +0100

    Use cl-letf instead of bundling noflet
    It's better not to bundle any third-party libraries, not even in
    "tests/".  Such copies tend to not ever get updated and might end
    up on the `load-path`.  `cl-letf` is noisier, than `noflet` or the
    old `flet`, but it's part of Emacs.

--- tests/google-this-test.el
+++ tests/google-this-test.el
@@ -2,7 +2,6 @@
 (add-to-list 'load-path (expand-file-name "../"))
 
 (require 'ert)
-(require 'noflet)
 (require 'google-this)
 
 ;; (ert-deftest google-this-insert-lucky-string-should-replace-region ()
@@ -11,17 +10,18 @@
 ;;     (save-excursion (insert " suffix"))
 ;;     (let ((left (point))
 ;;           (old-last-url google-this--last-url)
-;;           right)      
+;;           right)
 ;;       (insert "imdb raiders")
 ;;       (setq right (point))
-;;       (noflet ((region-active-p () t)
-;;                (region-beginning () left)
-;;                (region-end () right)
-;;                (read-string (_ s) s)
-;;                (kill-region (l r) (delete-region l r))
-;;                ;; Make this run synchronously, no need for manual search
-;;                (google-this--do-lucky-search (_ func)
-;;                  (funcall func "http://www.imdb.com/title/tt0082971/")))
+;;       (cl-letf (((symbol-function #'region-active-p) (lambda () t))
+;;                 ((symbol-function #'region-beginning) (lambda () left))
+;;                 ((symbol-function #'region-end) (lambda () right))
+;;                 ((symbol-function #'read-string) (lambda (_ s) s))
+;;                 ((symbol-function #'kill-region)
+;;                  (lambda (l r) (delete-region l r)))
+;;                 ((symbol-function #'google-this--do-lucky-search)
+;;                  (lambda (_ func)
+;;                    (funcall func "http://www.imdb.com/title/tt0082971/"))))
 ;;         (call-interactively 'google-this-lucky-and-insert-url)
 ;;         (setq google-this--last-url old-last-url)
 ;;         (should (equal "prefix http://www.imdb.com/title/tt0082971/ suffix"
@@ -37,22 +37,22 @@
 (defvar gtt--url-result-quoted (gtt--format-and-hexify (concat "\"" gtt--test-string "\"")))
 (ert-deftest google-this-string-test ()
   (let ((google-this-wrap-in-quotes nil))
-    (noflet ((browse-url (url) url))
+    (cl-letf (((symbol-function #'browse-url) (lambda (url) url)))
       (should (equal (google-this-string nil gtt--test-string t) gtt--url-result))
       (should (equal (google-this-string t gtt--test-string t)  gtt--url-result-quoted)))))
 (ert-deftest google-this-string-inverted-test ()
   (let ((google-this-wrap-in-quotes t))
-    (noflet ((browse-url (url) url))
+    (cl-letf (((symbol-function #'browse-url) (lambda (url) url)))
       (should (equal (google-this-string nil gtt--test-string t) gtt--url-result-quoted))
       (should (equal (google-this-string t gtt--test-string t) gtt--url-result)))))
 
 ;;; google-this-search tests
 (ert-deftest google-this-search-test ()
   (let ((google-this-wrap-in-quotes nil))
-    (noflet ((browse-url (url) url)
-              (google-this-pick-term (_) gtt--test-string))
-      (should (equal 
-                (google-this-search nil "foo *%s& url Tester") 
+    (cl-letf (((symbol-function #'browse-url) (lambda (url) url))
+              ((symbol-function #'google-this-pick-term) (lambda (_) gtt--test-string)))
+      (should (equal
+                (google-this-search nil "foo *%s& url Tester")
                 (format "foo *%s& url Tester" (url-hexify-string gtt--test-string)))))))
 
 ;;; Other google-this-something tests
@@ -65,24 +65,23 @@
       (insert "test\nI am doing")
       (save-excursion (insert " right\nnow"))
       (setq right (point))
-      (noflet
-       ((region-beginning () left)
-        (region-end () right)
-        (browse-url (url) url)
-        (read-string (_ def) def))
-       (noflet
-        ((region-active-p () t))
-        (should (equal (google-this-region nil) (google-this-noconfirm nil)))
-        (should (equal (google-this-region nil) (gtt--format-and-hexify "test\nI am doing")))
-        (should (equal (google-this-line nil) (gtt--format-and-hexify "I am doing right")))
-        (should (equal (google-this-word nil) (gtt--format-and-hexify "doing")))
-        (should (equal (google-this-symbol nil) (gtt--format-and-hexify "doing"))))
-       (should (equal (google-this-word nil) (google-this-noconfirm nil)))
-       (should (equal (google-this-symbol nil) (google-this-noconfirm nil)))
-       (goto-char (line-end-position))
-       (insert " ")
-       (should (equal (google-this-line nil) (google-this-noconfirm nil)))))))
+      (cl-letf (((symbol-function #'region-beginning) (lambda () left))
+                ((symbol-function #'region-end) (lambda () right))
+                ((symbol-function #'browse-url) (lambda (url) url))
+                ((symbol-function #'read-string) (lambda (_ def) def)))
+        (cl-letf (((symbol-function #'region-active-p) (lambda () t)))
+          (should (equal (google-this-region nil) (google-this-noconfirm nil)))
+          (should (equal (google-this-region nil) (gtt--format-and-hexify "test\nI am doing")))
+          (should (equal (google-this-line nil) (gtt--format-and-hexify "I am doing right")))
+          (should (equal (google-this-word nil) (gtt--format-and-hexify "doing")))
+          (should (equal (google-this-symbol nil) (gtt--format-and-hexify "doing"))))
+        (should (equal (google-this-word nil) (google-this-noconfirm nil)))
+        (should (equal (google-this-symbol nil) (google-this-noconfirm nil)))
+        (goto-char (line-end-position))
+        (insert " ")
+        (should (equal (google-this-line nil) (google-this-noconfirm nil)))))))
 
 ;;; Local Variables:
-;;; lisp-indent-function:common-lisp-indent-function
+;;; indent-tabs-mode: nil
+;;; lisp-indent-function: common-lisp-indent-function
 ;;; End:
